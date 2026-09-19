cb8227c4a1f556a56c22325fe7b27c90715058ad
Somyot Thanimwas <plaifaeng@gmail.com>
2026-09-19 19:25:14 +0700

    Prevent stale stock data caching

--- backend/main.cpp
+++ backend/main.cpp
@@ -237,6 +237,16 @@ response.set(
 );
 
 response.set(
+    http::field::cache_control,
+    "no-store, no-cache, must-revalidate"
+);
+
+response.set(
+    http::field::pragma,
+    "no-cache"
+);
+
+response.set(
     http::field::access_control_allow_origin,
     "*"
 );
@@ -317,7 +327,8 @@ const std::string& csv
     const std::string url =
         "https://raw.githubusercontent.com/"
         "Somyotthanimwas/FundamentalWeb/main/"
-        "data/fundamental_v4.csv";
+        "data/fundamental_v4.csv?cache_bust=" +
+        std::to_string(std::time(nullptr));
 
     const std::string tmp = csv + ".github.tmp";
 
